59c27c906ba960e87cc323cd7b6f44aeacfd583a
elianajr <elianajr1@gmail.com>
2021-10-26 20:55:23 +0000

    con css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Counter from "./Counter.jsx";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 //include images into your bundle
 
 //create your first component
 const Home = () => {
 	return (
-		<div class="timer">
-			<Counter time="1000" />
-			<Counter time="10000" />
-			<Counter time="100000" />
-			<Counter time="1000000" />
+		<div className="container">
+			<div className="text">
+				<Counter className="item1" time="1000000" />
+				<Counter className="item2" time="100000" />
+				<Counter className="item3" time="10000" />
+				<Counter className="item4" time="1000" />
+			</div>
 		</div>
 	);
 };
